5ae1ad523f38e73c886fc8a86dc5c9b59d14979d
Your Name <michaelalex03@outlook.com>
2024-04-09 18:46:36 -0500

    Got api request working from OpenWeatherApi and displayed temperature for given city and statecode

--- index.js
+++ index.js
@@ -8,14 +8,24 @@ app.use(express.static("./public"));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+const apiKey = "a2f791935cc39ad4fcc5ab8eb4a53378";
+var temperature;
+
 app.get('/', (req, res) => {
     res.render("index.ejs");
   });
 
 app.get("/search", async(req, res) => {
+  const city = req.query.city;
+  const stateCode = req.query.statecode;
+  console.log(city)
+  console.log(stateCode)
   try {
-      const result = await axios.get(API_URL + "/random");
-      res.render("index.ejs", { secret: JSON.stringify(result.data.secret), user: JSON.stringify(result.data.username)});
+      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${stateCode},&appid=${apiKey}`);
+      console.log(result);
+      temperature = (result.data.main.temp * (9/5)) - 459.67;
+      temperature = temperature.toFixed(0);
+      res.render("index.ejs", { temp: temperature});
     } catch (error) {
       res.render("index.ejs", { secret: JSON.stringify(error.response.data)});
     }
