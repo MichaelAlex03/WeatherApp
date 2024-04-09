0395e2ab7278644c9c5ece55844ac06ef51286d2
Your Name <michaelalex03@outlook.com>
2024-04-09 14:52:14 -0500

    first commit

--- index.js
+++ index.js
@@ -6,9 +6,20 @@ const port = 3000;
 
 app.use(express.static("./public"));
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
     res.render("index.ejs");
   });
+
+app.get("/search", async(req, res) => {
+  try {
+      const result = await axios.get(API_URL + "/random");
+      res.render("index.ejs", { secret: JSON.stringify(result.data.secret), user: JSON.stringify(result.data.username)});
+    } catch (error) {
+      res.render("index.ejs", { secret: JSON.stringify(error.response.data)});
+    }
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
