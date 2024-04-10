2c0e76aeebf94852939d394b917996f8f3dd0506
Your Name <michaelalex03@outlook.com>
2024-04-10 18:37:00 -0500

    another commit

--- index.js
+++ index.js
@@ -11,27 +11,43 @@ app.use(bodyParser.urlencoded({extended: true}));
 const apiKey = "a2f791935cc39ad4fcc5ab8eb4a53378";
 var temperature;
 var currentTime = new Date();
-var currentHours = currentTime.getHours();
-var currentMin = currentTime.getMinutes();
+var time = currentTime.getHours() + ":" + currentTime.getMinutes();
+
+function convertToNonMilitaryTime(req, res, next) {
+  var hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
+  var period = (hours >= 12) ? 'PM' : 'AM';
+  if (hours === 0) {
+    hours = 12;
+  } else if (hours > 12) {
+    hours -= 12;
+  }
+  time = hours + ':' + (minutes < 10 ? '0' : '') + minutes + ' ' + period;
+  next(); 
+}
+
+app.use(convertToNonMilitaryTime);
 
 app.get('/', (req, res) => {
     res.render("index.ejs");
   });
 
-  app.get("/search", async(req, res) => {
-    const city = req.query.city;
-    const stateCode = req.query.statecode;
-    try {
-        const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${stateCode},&appid=${apiKey}`);
-        console.log(result);
-        temperature = (result.data.main.temp * (9/5)) - 459.67;
-        temperature = temperature.toFixed(0);
-        res.render("index.ejs", { temp: temperature, weatherId: result.data.weather[0].id, time: time});
-      } catch (error) {
-        res.render("index.ejs", { secret: JSON.stringify(error.response.data)});
-      }
-  });
-  
+app.get("/search", async(req, res) => {
+  var city = req.query.city;
+  var stateCode = req.query.statecode;
+  city = city[0].toUpperCase() + city.slice(1)
+  stateCode = stateCode.toUpperCase();
+  try {
+      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${stateCode},&appid=${apiKey}`);
+      console.log(result.data.weather[0]);
+      temperature = (result.data.main.temp * (9/5)) - 459.67;
+      temperature = temperature.toFixed(0);
+      res.render("index.ejs", { temp: temperature, weatherId: result.data.weather[0].id, time: time, city: city, state: stateCode});
+    } catch (error) {
+      res.render("index.ejs", { secret: JSON.stringify(error.response.data)});
+    }
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
