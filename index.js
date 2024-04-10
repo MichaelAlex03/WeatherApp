b124647235685be211e89ef3fcfd53a028bdb45f
MichaelAlex03 <michaelalex03@outlook.com>
2024-04-10 16:26:29 -0500

    another commit

--- index.js
+++ index.js
@@ -10,27 +10,28 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const apiKey = "a2f791935cc39ad4fcc5ab8eb4a53378";
 var temperature;
+var currentTime = new Date();
+var currentHours = currentTime.getHours();
+var currentMin = currentTime.getMinutes();
 
 app.get('/', (req, res) => {
     res.render("index.ejs");
   });
 
-app.get("/search", async(req, res) => {
-  const city = req.query.city;
-  const stateCode = req.query.statecode;
-  console.log(city)
-  console.log(stateCode)
-  try {
-      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${stateCode},&appid=${apiKey}`);
-      console.log(result);
-      temperature = (result.data.main.temp * (9/5)) - 459.67;
-      temperature = temperature.toFixed(0);
-      res.render("index.ejs", { temp: temperature});
-    } catch (error) {
-      res.render("index.ejs", { secret: JSON.stringify(error.response.data)});
-    }
-});
-
+  app.get("/search", async(req, res) => {
+    const city = req.query.city;
+    const stateCode = req.query.statecode;
+    try {
+        const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${stateCode},&appid=${apiKey}`);
+        console.log(result);
+        temperature = (result.data.main.temp * (9/5)) - 459.67;
+        temperature = temperature.toFixed(0);
+        res.render("index.ejs", { temp: temperature, weatherId: result.data.weather[0].id, time: time});
+      } catch (error) {
+        res.render("index.ejs", { secret: JSON.stringify(error.response.data)});
+      }
+  });
+  
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
